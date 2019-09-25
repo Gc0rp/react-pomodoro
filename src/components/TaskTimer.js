5191e06ebd2c7220085df079df2c95f98060467b
Gc0rp <ojasgupta4321@gmail.com>
2019-09-25 21:41:09 +1000

    Started working on the timer.

--- src/components/TaskTimer.js
+++ src/components/TaskTimer.js
@@ -1,35 +1,65 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const Timer = styled.h3`
+font-family: 'Cantarell', sans-serif;
+color: #FF8552;
+font-size: 20px;
+font-weight: 800;
+padding-top: 20px;
+`;
+
 
 class TaskTimer extends React.Component{ 
 
     constructor(props){
         super(props);
         this.state = {
-            time: this.props.taskTime,
-            seconds: 60
+            minutes: this.props.taskTime,
+            seconds: 0,
+            pause: false
         };
 
-        this.decreaseSeconds = this.decreaseSeconds.bind(this);
+        this.startTime = this.startTime.bind(this);
+        this.stopTime = this.stopTime.bind(this);
+
+        var timer;
     }
 
-    componentDidMount(){
-        setInterval(this.decreaseSeconds, 1000);
-        
+    startTime(){
+        if(!this.state.pause) {
+            this.timer = setInterval( () => {
+                if(this.state.seconds > 0) {
+                    this.setState({
+                        seconds: this.state.seconds - 1
+                    });
+                } else if (this.state.minutes > 0) {
+                    this.setState({
+                        seconds: 59,
+                        minutes: this.state.minutes - 1
+                    });
+                } else {
+                    this.stopTime();
+                }
+            }, 1000);
+        }
     }
 
-    decreaseSeconds(){
-        console.log("function called");
-        this.setState = {
-            seconds: this.state.seconds - 1
-        };
-
-        console.log("Seconds is now: " + this.state.seconds);
+    stopTime(){
+        this.setState({
+            pause: true
+        });
+        clearInterval(this.timer);
     }
 
     render(){
         return(
-            <p>{this.state.seconds}</p>
-
+            <div>
+                <Timer>{this.state.minutes} : {this.state.seconds}</Timer>
+                <button type="button" onClick={this.startTime}>Start</button> 
+                <button type="button" onClick={this.stopTime}>Stop</button> 
+            </div> 
         );
     };
 };
